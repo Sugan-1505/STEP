537bc54147e1d8abc4dc9bd7cbdad52c9ff4a510
Sugan-1505 <suganprasad9@gmail.com>
2026-09-19 14:15:19 +0530

    Create Nickname Tag

---
+++ practice problem/Nickname_Tag.java
@@ -0,0 +1,21 @@
+public class Nickname_Tag {
+    static class NameTag {
+        private final String firstName;
+        private final String lastName;
+
+        NameTag(String fullName) {
+            String[] parts = fullName.split(" ");
+            firstName = parts[0];
+            lastName = parts[1];
+        }
+
+        public String getNickname() {
+            return firstName + " " + lastName.charAt(0) + ".";
+        }
+    }
+
+    public static void main(String[] args) {
+        NameTag tag = new NameTag("Maria Gomez");
+        System.out.println("Nickname: " + tag.getNickname());
+    }
+}
